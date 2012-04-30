2276d463975c14a624b8fdb4c9a8007c25256692
Mariusz <mariusz@balewski.eu>
2012-04-30 16:37:50 +0200

    Added functions to: 1. Modify entry 2. Check if entry exists Added events logging.

--- budget/src/config.c
+++ budget/src/config.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "config.h"
 #include "db.h"
+#include "log.h"
 
 int add_config(char *param_name, char *value) {
 	sqlite3 *conn;
@@ -14,11 +15,36 @@ int add_config(char *param_name, char *value) {
 	char *sql = sqlite3_mprintf("INSERT INTO config(param, value) VALUES('%s', '%s')", param_name, value);
 	error = sqlite3_exec(conn, sql, 0, 0, 0);
 	if (error != SQLITE_OK) {
-		printf("ERROR: %d\n",error);
+		char *msg = malloc(255);
+		sprintf(msg, "add_config: SQL Error(%d): %s", error, sqlite3_errmsg(conn));
+		printf("%s\n", msg);
+		free(msg);
+		return error;
 	}
 	sqlite3_close(conn);
 	sqlite3_free(sql);
 	return error;
+}
+
+int update_config(char *param_name, char *value) {
+	sqlite3 *conn;
+	int error = 0;
+	error = sqlite3_open(DB_FILE, &conn);
+	check_db_open(error);
+
+	char *sql = sqlite3_mprintf("UPDATE config SET value='%s' WHERE param='%s'", value, param_name);
+	error = sqlite3_exec(conn, sql, 0, 0, 0);
+	if (error != SQLITE_OK) {
+		char *msg = malloc(255);
+		sprintf(msg, "update_config: SQL Error(%d): %s", error, sqlite3_errmsg(conn));
+		printf("%s\n", msg);
+		free(msg);
+		return error;
+	}
+	sqlite3_close(conn);
+	sqlite3_free(sql);
+	return error;
+
 }
 
 int get_config(char *param_name, char *value) {
@@ -28,19 +54,58 @@ int get_config(char *param_name, char *value) {
 	int error = 0;
 	error = sqlite3_open(DB_FILE, &conn);
 	if (error) {
-		puts("Can't open database");
+		char *msg = malloc(255);
+		sprintf(msg, "get_config: SQL Error(%d): %s", error, sqlite3_errmsg(conn));
+		printf("%s\n", msg);
+		free(msg);
 		return error;
 	}
 	char *sql = sqlite3_mprintf("SELECT value FROM config WHERE param='%s'", param_name);
 	error = sqlite3_prepare_v2(conn, sql, -1, &res, &tail);
 
 	if (error != SQLITE_OK) {
-		printf("ERROR: %d\n", error);
+		char *msg = malloc(255);
+		sprintf(msg, "get_config: SQL Error(%d): %s", error, sqlite3_errmsg(conn));
+		printf("%s\n", msg);
+		free(msg);
 		return error;
 	}
 
 	while (sqlite3_step(res) == SQLITE_ROW) {
 		strcpy(value, (char*)sqlite3_column_text(res, 0));
+	}
+	sqlite3_finalize(res);
+	sqlite3_close(conn);
+	sqlite3_free(sql);
+	return error;
+}
+
+int config_exists(char *param_name, int *count) {
+	sqlite3 *conn;
+	sqlite3_stmt *res;
+	const char *tail;
+	int error = 0;
+	error = sqlite3_open(DB_FILE, &conn);
+	if (error) {
+		char *msg = malloc(255);
+		sprintf(msg, "config_exists: SQL Error(%d): %s", error, sqlite3_errmsg(conn));
+		printf("%s\n", msg);
+		free(msg);
+		return error;
+	}
+	char *sql = sqlite3_mprintf("SELECT count(*) FROM config WHERE param='%s'", param_name);
+	error = sqlite3_prepare_v2(conn, sql, -1, &res, &tail);
+
+	if (error != SQLITE_OK) {
+		char *msg = malloc(255);
+		sprintf(msg, "config_exists: SQL Error(%d): %s", error, sqlite3_errmsg(conn));
+		printf("%s\n", msg);
+		free(msg);
+		return error;
+	}
+
+	while (sqlite3_step(res) == SQLITE_ROW) {
+		*count = (int)sqlite3_column_int(res, 0);
 	}
 	sqlite3_finalize(res);
 	sqlite3_close(conn);
--- budget/src/config.h
+++ budget/src/config.h
@@ -3,4 +3,6 @@
 
 int add_config(char *param_name, char *value);
 int get_config(char *param_name, char *value);
+int update_config(char *param_name, char *value);
+int config_exists(char *param_name, int *count);
 #endif
